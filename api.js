9a8dea7db5581e6e183c076c05c3fd309373a810
JorgeSoberanis <jsoberanisbalam@gmail.com>
2022-03-18 18:34:03 -0600

    pokedex solo img

--- api.js
+++ api.js
@@ -1,13 +1,31 @@
-const fetchPokemon = () =>{
-    const pokeName = document.getElementById("name")
-    let pokeInput = pokeName.valur.toLowercase();
+const fetchPokemon = () => {
+    const pokeName = document.getElementById("pokeName");
+    let pokeInput = pokeName.value
     const url = `https://pokeapi.co/api/v2/pokemon/${pokeInput}`;
+    fetch(url).then((res) => {
+        //console.log(res)
+        return res.json();
+    }).then((data) =>{
+        console.log(data);
+        let pokeImg = data.sprites.front_default; //asi se accede a los diferetes valores del json
+        console.log(pokeImg);
+        pokeImage(pokeImg);
+    })
 }
+
 
 const pokeImage = (url) =>{
-
-    const pokeImg = document.getElementById("resultado")
+    const pokeImg =  document.getElementById ("pokeImg");
     pokeImg.src = url;
+}
 
+//pokeImage("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png");
+
+const imprimir = () =>{
+    const pokeName = document.getElementById("pokeName");
+    let pokeInput = pokeName.value
+    console.log("wuenas " + pokeInput);
 
 }
+
+//fetchPokemon();
